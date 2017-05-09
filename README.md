ec7b09897af14a3ebbfbc37be2f232e271714312
bovenson <szhkai@126.com>
2017-05-09 18:29:19 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 东北大学IP网管登录注销脚本
+##使用
+- 将`config-sample.ini`重命名为`config.ini`
+- 修改`config.ini`文件内用户名和密码
+- 运行脚本
+## 登录
+```shell
+python3 login.py
+```
+## 注销
+```shell
+python3 logout.py
+```
---
+++ logout.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+from urllib import request, parse
+from configparser import ConfigParser
+
+
+# 从配置文件读取账号密码
+cf = ConfigParser()
+cf.read('config.ini')
+username = cf.get('account', 'username')
+password = cf.get('account', 'password')
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36',
+    # 'Host': 'ipgw.neu.edu.cn',
+    # 'Origin': 'https://ipgw.neu.edu.cn',
+    # 'Referer': 'https://ipgw.neu.edu.cn/srun_portal_pc.php?url=&ac_id=1',
+}
+
+
+def login():
+    url = 'https://ipgw.neu.edu.cn/srun_portal_pc.php?url=&ac_id=1'
+
+    params = {
+        'action': 'login',
+        'ac_id': 1,
+        'user_ip': '',
+        'nas_ip': '',
+        'user_mac': '',
+        'url': '',
+        'username': username,
+        'password': password,
+        'save_me': 0,
+    }
+
+    data = parse.urlencode(params).encode(encoding='utf-8')
+    req = request.Request(url=url, data=data, headers=headers, method='POST')
+    res = request.urlopen(req, timeout=15)
+    if res.getcode() != 200:
+        print('操作失败')
+    else:
+        print('操作成功')
+    # else:
+    #     print(str(res.read(), encoding='utf-8'))
+
+
+def logout():
+    url = 'https://ipgw.neu.edu.cn/include/auth_action.php'
+
+    params = {
+        'action': 'logout',
+        'username': username,
+        'password': password,
+        'ajax': '1',
+    }
+
+    data = parse.urlencode(params).encode(encoding='utf-8')
+    req = request.Request(url=url, data=data, headers=headers, method='POST')
+    res = request.urlopen(req, timeout=15)
+    if res.getcode() != 200:
+        print('操作失败')
+    else:
+        print('操作成功')
+    # else:
+    #     print(str(res.read(), encoding='utf-8'))
+
+
+if __name__ == "__main__":
+    logout()
