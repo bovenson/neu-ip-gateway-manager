78ab07cc85a2aabd499fdd41746d98f14c91999f
bovenson <szhkai@qq.com>
2017-09-21 10:18:35 +0800

    >relogin.sh; + cron config file

---
+++ auto-login.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 周期ping百度等, 不通重连
+v="This is a variable."
+
+echo $v
+echo ${v}
--- relogin.sh
+++ relogin.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+FULLPATH="$PWD/$(dirname $0)"
+cd $FULLPATH
 python3 logout.py
 python3 login.py
